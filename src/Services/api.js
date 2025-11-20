18cc9b83cb850e1f1dc91ff46293467cbf9023a3
ttmagedara2001 <thulanimagedara@gmail.com>
2025-11-20 15:04:18 +0530

    modify: Protonest Connect base URL added

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,10 +1,19 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', 
+  baseURL: 'https://protonest-connect-general-app.yellowsea-5dc9141a.westeurope.azurecontainerapps.io/api/v1/user',
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+// Add token to requests if available
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwtToken');
+  if (token) {
+    config.headers['X-Token'] = token;
+  }
+  return config;
 });
 
 export default api;
