62eae1686e4304f3a400dfbbefbc2d47755bbfd5
ttmagedara2001 <thulanimagedara@gmail.com>
2025-11-20 16:08:52 +0530

    modify: removed comments

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -2,7 +2,6 @@ import api from './api';
 
 export const loginUser = async (email, password) => {
   try {
-    // Source 3: POST /get-token
     const response = await api.post('/get-token', { 
       email, 
       password 
@@ -10,7 +9,6 @@ export const loginUser = async (email, password) => {
 
     if (response.data.status === "Success") {
       const { jwtToken, refreshToken } = response.data.data;
-      // Save tokens
       localStorage.setItem('jwtToken', jwtToken);
       localStorage.setItem('refreshToken', refreshToken);
       return response.data.data;
