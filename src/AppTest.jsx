8eddff3bbe8934aea0595d62d822e9d67150c368
ttmagedara2001 <thulanimagedara@gmail.com>
2025-11-21 13:10:31 +0530

    modify: modified for WebSocket Test Hook

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './Components/DashboardTest';
 import ErrorBoundary from './Components/ErrorBoundary';
 
-
+// Modified for WebSocket Test Hook
 function App() {
   return (
     <ErrorBoundary>
