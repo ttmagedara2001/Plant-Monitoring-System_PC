49dae2f16ba8f37b650f81f70de469990a4eb891
ttmagedara2001 <thulanimagedara@gmail.com>
2025-11-21 13:06:56 +0530

    modify: syntax

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,16 +1,16 @@
-import api from './api';
+import api from "./apiTest";
 
 export const loginUser = async (email, password) => {
   try {
-    const response = await api.post('/get-token', { 
-      email, 
-      password 
+    const response = await api.post("/get-token", {
+      email,
+      password,
     });
 
     if (response.data.status === "Success") {
       const { jwtToken, refreshToken } = response.data.data;
-      localStorage.setItem('jwtToken', jwtToken);
-      localStorage.setItem('refreshToken', refreshToken);
+      localStorage.setItem("jwtToken", jwtToken);
+      localStorage.setItem("refreshToken", refreshToken);
       return response.data.data;
     } else {
       throw new Error(response.data.data || "Login Failed");
